df5b256c787f9fa649f17450975bf1756cb022d9
Adib Setiawan <itsmeadib.s@gmail.com>
2022-06-24 21:06:00 +0700

    make async

---
+++ promise.js
@@ -0,0 +1,23 @@
+function readBooksPromise(time, book) {
+  console.log("saya mulai membaca " + book.name);
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      let sisaWaktu = time - book.timeSpent;
+      if (sisaWaktu >= 0) {
+        console.log("saya sudah selesai membaca " + book.name + ", sisa waktu saya " + sisaWaktu);
+        resolve(sisaWaktu);
+      } else {
+        console.log("saya sudah tidak punya waktu untuk baca " + book.name);
+        reject(sisaWaktu);
+      }
+    }, book.timeSpent);
+  });
+}
+
+var books = [
+  { name: "LOTR", timeSpent: 3000 },
+  { name: "Fidas", timeSpent: 2000 },
+  { name: "Kalkulus", timeSpent: 4000 },
+];
+
+readBooksPromise(10000, books[1])
